7026c425a3f6c5cff23a4512e481ab98ee2f568c
Tuğba Kapucu <tugba.kapucu@sahabt.com>
2021-01-31 16:22:56 +0300

    update

--- Malatya/Program.cs
+++ Malatya/Program.cs
@@ -17,6 +17,7 @@ namespace Malatya
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            var a = "";
         }
     }
 }
